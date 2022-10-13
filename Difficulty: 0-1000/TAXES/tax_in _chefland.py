42460b6b60d3150523563dec57aa938c62231b04
Tanisha Yadav <105712733+26-tanisha@users.noreply.github.com>
2022-10-13 20:15:43 +0530

    Update tax_in _chefland.py

--- Difficulty: 0-1000/TAXES/tax_in _chefland.py
+++ Difficulty: 0-1000/TAXES/tax_in _chefland.py
@@ -1,1 +1,6 @@
-
+for i in range(int(input())): #taking input for t test cases
+    x = int(input()) #input income
+    if(x>100):
+        print("{}".format(x-10)) #deducting rs10 from x if its greater than 100
+    else:
+        print(x)
